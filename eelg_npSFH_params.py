7abef387c8114d4583738654090b301d39402b56
jhcohn <joncohn@tamu.edu>
2017-04-26 22:27:09 -0500

    cleanup

--- eelg_npSFH_params.py
+++ eelg_npSFH_params.py
@@ -12,7 +12,7 @@ logarithmic = priors.logarithmic
 # RUN_PARAMS
 #############
 
-id = str(2329)  # 1824
+id = str(1824)  # 1824 (eelg), 2329 (normal)
 
 run_params = {'verbose': True,
               'debug': False,
@@ -36,6 +36,7 @@ run_params = {'verbose': True,
               'photname': '/home/jonathan/cosmos/cosmos.v1.3.8.cat',
               'datname': '/home/jonathan/cosmos/cosmos.v1.3.8.cat',
               'fastname': '/home/jonathan/cosmos/cosmos.v1.3.6.awk.fout',  # .fout edited to correct format using awk
+              'zname': '/home/jonathan/cosmos/cosmos.v1.3.6.awk.zout',  # main cat has z_spec, but not z_phot
               'objname': id,  # 1824
               }
 run_params['outfile'] = run_params['outfile'] + '_' + run_params['objname']
@@ -218,16 +219,16 @@ model_params.append({'name': 'tau', 'N': 1,
                      'prior_args': {'mini': 0.1,
                                     'maxi': 100.0}})
 
-model_params.append({'name': 'logtau', 'N': 1,  # NEW TURN OFF -- HOW? SET N: 0?
-                        'isfree': True,
+model_params.append({'name': 'logtau', 'N': 1,
+                        'isfree': False,  # NEW turn off
                         'init': 1,
                         'init_disp': 0.5,
                         'units': 'Gyr',
                         'prior_function':tophat,
                         'prior_args': {'mini':-1, 'maxi':2}})
 
-model_params.append({'name': 'tage', 'N': 1,  # NEW TURN OFF -- HOW? SET N: 0?
-                     'isfree': True,
+model_params.append({'name': 'tage', 'N': 1,
+                     'isfree': False,  # NEW turn off
                      'init': 1.0,
                      'units': 'Gyr',
                      'prior_function': tophat,
@@ -380,6 +381,9 @@ class BurstyModel(sedmodel.SedModel):  # NEW, replacing below
         lnp_prior = 0
 
         # sum of SFH fractional bins <= 1.0
+        print(self.theta_index, 'theta_index')
+        print(theta, 'theta')
+        # print(theta[0], 'sfr_theta_index')
         if 'sfr_fraction' in self.theta_index:
             sfr_fraction = theta[self.theta_index['sfr_fraction']]
             if np.sum(sfr_fraction) > 1.0:
@@ -425,32 +429,6 @@ class BurstyModel(sedmodel.SedModel):  #old, replaced by above
 
 
 class FracSFH(FastStepBasis):  # NEW CLASS
-    @property
-    def emline_wavelengths(self):
-        return self.ssp.emline_wavelengths
-
-    @property
-    def get_nebline_luminosity(self):
-        """Emission line luminosities in units of Lsun per solar mass formed
-        """
-        return self.ssp.emline_luminosity / self.params['mass'].sum()
-
-    def nebline_photometry(self, filters, z):
-        """analytically calculate emission line contribution to photometry
-        """
-        emlams = self.emline_wavelengths * (1 + z)
-        elums = self.get_nebline_luminosity  # Lsun / solar mass formed
-        flux = np.empty(len(filters))
-        for i, filt in enumerate(filters):
-            # calculate transmission at nebular emission
-            trans = np.interp(emlams, filt.wavelength, filt.transmission, left=0., right=0.)
-            idx = (trans > 0)
-            if True in idx:
-                flux[i] = (trans[idx] * emlams[idx] * elums[idx]).sum() / filt.ab_zero_counts
-            else:
-                flux[i] = 0.0
-        return flux
-
     def get_galaxy_spectrum(self, **params):
         self.update(**params)
 
@@ -472,143 +450,51 @@ class FracSFH(FastStepBasis):  # NEW CLASS
 
         return wave, spec / mtot, self.ssp.stellar_mass / mtot
 
-    def get_spectrum(self, outwave=None, filters=None, peraa=False, **params):
-        """Get a spectrum and SED for the given params.
-        ripped from SSPBasis
-        addition: check for flag nebeminspec. if not true,
-        add emission lines directly to photometry
-        """
-
-        # Spectrum in Lsun/Hz per solar mass formed, restframe
-        wave, spectrum, mfrac = self.get_galaxy_spectrum(**params)
-
-        # Redshifting + Wavelength solution
-        # We do it ourselves.
-        a = 1 + self.params.get('zred', 0)
-        af = a
-        b = 0.0
-
-        if 'wavecal_coeffs' in self.params:
-            x = wave - wave.min()
-            x = 2.0 * (x / x.max()) - 1.0
-            c = np.insert(self.params['wavecal_coeffs'], 0, 0)
-            # assume coeeficients give shifts in km/s
-            b = chebval(x, c) / (lightspeed * 1e-13)
-
-        wa, sa = wave * (a + b), spectrum * af  # Observed Frame
-        if outwave is None:
-            outwave = wa
-
-        spec_aa = lightspeed / wa ** 2 * sa  # convert to perAA
-        # Observed frame photometry, as absolute maggies
-        if filters is not None:
-            mags = observate.getSED(wa, spec_aa * to_cgs, filters)
-            phot = np.atleast_1d(10 ** (-0.4 * mags))
-        else:
-            phot = 0.0
-
-        ### if we don't have emission lines, add them
-        if (not self.params['nebemlineinspec']) and self.params['add_neb_emission']:
-            phot += self.nebline_photometry(filters, a - 1) * to_cgs
-
-        # Spectral smoothing.
-        do_smooth = (('sigma_smooth' in self.params) and
-                     ('sigma_smooth' in self.reserved_params))
-        if do_smooth:
-            # We do it ourselves.
-            smspec = self.smoothspec(wa, sa, self.params['sigma_smooth'],
-                                     outwave=outwave, **self.params)
-        elif outwave is not wa:
-            # Just interpolate
-            smspec = np.interp(outwave, wa, sa, left=0, right=0)
-        else:
-            # no interpolation necessary
-            smspec = sa
-
-        # Distance dimming and unit conversion
-        zred = self.params.get('zred', 0.0)
-        if (zred == 0) or ('lumdist' in self.params):
-            # Use 10pc for the luminosity distance (or a number
-            # provided in the dist key in units of Mpc)
-            dfactor = (self.params.get('lumdist', 1e-5) * 1e5) ** 2
-        else:
-            lumdist = WMAP9.luminosity_distance(zred).value
-            dfactor = (lumdist * 1e5) ** 2
-        if peraa:
-            # spectrum will be in erg/s/cm^2/AA
-            smspec *= to_cgs / dfactor * lightspeed / outwave ** 2
-        else:
-            # Spectrum will be in maggies
-            smspec *= to_cgs / dfactor / 1e3 / (3631 * jansky_mks)
-
-        # Convert from absolute maggies to apparent maggies
-        phot /= dfactor
-
-        # Mass normalization
-        mass = np.sum(self.params.get('mass', 1.0))
-        if np.all(self.params.get('mass_units', 'mstar') == 'mstar'):
-            # Convert from current stellar mass to mass formed
-            mass /= mfrac
-
-        return smspec * mass, phot * mass, mfrac
-
 def load_sps(**extras):
     sps = FracSFH(**extras)  # new, replaced CSPBasis
     return sps
 
 
-def load_model(objname='', datname='', fastname='', agelims=[], **extras):
+def load_model(objname='', datname='', zname='', agelims=[], **extras):
     ###### REDSHIFT ######
     ### open file, load data
-    '''
+
     with open(datname, 'r') as f:
         hdr = f.readline().split()
     dtype = np.dtype([(hdr[1],'S20')] + [(n, np.float) for n in hdr[2:]])
     dat = np.loadtxt(datname, comments = '#', delimiter=' ',
                      dtype = dtype)
-    '''
-    with open(fastname, 'r') as f:
-        hdr = f.readline().split()
-    dtype = np.dtype([(hdr[1], 'S20')] + [(n, np.float) for n in hdr[2:]])
-    fast = np.loadtxt(fastname, comments='#', delimiter=' ', dtype=dtype)
-    idx = fast['id'] == objname
-    zred = fast['z'][idx][0]
 
-    #### NONPARAMETRIC SFH ######  # NEW
-    agelims[-1] = np.log10(tuniv*1e9)
-    agebins = np.array([agelims[:-1], agelims[1:]])
-    ncomp = len(agelims) - 1
-    agelims = [0.0, 7.0, 8.0, (8.0 + (np.log10(tuniv*1e9)-8.0)/4), (8.0 + 2*(np.log10(tuniv*1e9)-8.0)/4),
-               (8.0 + 3*(np.log10(tuniv*1e9)-8.0)/4), np.log10(tuniv*1e9)]
-    # calculate the somethings: [0, a, b, b + (f-b)/4, b + 2*(f-b)/4, b + 3*(f-b)/4, b + 4*(f-b)/4 = f]
+    with open(zname, 'r') as fz:
+        hdr_z = fz.readline().split()
+    dtype_z = np.dtype([(hdr_z[1],'S20')] + [(n, np.float) for n in hdr_z[2:]])
+    zout = np.loadtxt(zname, comments = '#', delimiter=' ', dtype = dtype_z)
 
-    #### INITIAL VALUES
-    logtau = np.log10(10 ** fast['ltau'][idx][0] / 1e9)
-    # tau = 10 ** fast['ltau'][idx][0] / 1e9  # Prospector does tau in units of Gyr, FAST returns log(tau) in yr
-    tage = 10 ** fast['lage'][idx][0] / 1e9
-    logmass = fast['lmass'][idx][0]
-    dust2 = fast['Av'][idx][0] / 1.086
+    idx = dat['id'] == objname
+    zred = zout['z_spec'][idx][0]  # BUCKET FIX THE INDEX HERE
+    if zred == -99:
+        zred = zout['z_peak'][idx][0]
 
-    print(tage, 'tage')
-
-    # print('z', 'tau', 'tage', 'logmass', 'dust')
-    # print(zred, tau, tage, logmass, dust2)
-
-    n = [p['name'] for p in model_params]
-    model_params[n.index('logtau')]['init'] = logtau
-    model_params[n.index('tage')]['init'] = tage
-    model_params[n.index('logmass')]['init'] = logmass
-    model_params[n.index('dust2')]['init'] = dust2
+    print(zred, 'zred')
 
     #### CALCULATE TUNIV #####
     tuniv = WMAP9.age(zred).value
     print(tuniv, 'tuniv')
+
+    n = [p['name'] for p in model_params]
     model_params[n.index('tage')]['prior_args']['maxi'] = tuniv
+
+    #### NONPARAMETRIC SFH ######  # NEW
+#     agelims[-1] = np.log10(tuniv*1e9)
+    ncomp = len(agelims) - 1
+    agelims = [0.0, 7.0, 8.0, (8.0 + (np.log10(tuniv*1e9)-8.0)/4), (8.0 + 2*(np.log10(tuniv*1e9)-8.0)/4),
+               (8.0 + 3*(np.log10(tuniv*1e9)-8.0)/4), np.log10(tuniv*1e9)]
+    agebins = np.array([agelims[:-1], agelims[1:]])
+    # calculate the somethings: [0, a, b, b + (f-b)/4, b + 2*(f-b)/4, b + 3*(f-b)/4, b + 4*(f-b)/4 = f]
 
     #### INSERT REDSHIFT INTO MODEL PARAMETER DICTIONARY ####
     zind = n.index('zred')
     model_params[zind]['init'] = zred
-
 
     #### SET UP AGEBINS
     model_params[n.index('agebins')]['N'] = ncomp
