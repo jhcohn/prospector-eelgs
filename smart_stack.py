bda3f155e251988eb77107d92c9e375a2e4e9eb9
jhcohn <joncohn@tamu.edu>
2017-08-29 17:05:29 -0500

    err options

--- smart_stack.py
+++ smart_stack.py
@@ -53,7 +53,7 @@ def smooth(perc):
     return smoother
 
 
-def stacker(gal_draws):
+def stacker(gal_draws, sigma=1):
     """
     stacker takes input of random points drawn from a list of galaxies' SFH posteriors, concatenates them within each
     bin, and then calculates the median and 1 sigma errors in each bin
@@ -61,6 +61,7 @@ def stacker(gal_draws):
     draws should be in format draws = [draw_from_sfh1, draw_from_sfh2, ...], where each draw_from_sfh has shape=(22,num)
 
     :param gal_draws: list comprised of draw_from_sfh (output from randraw) for a set of galaxies
+    :param sigma: how many sigma of error we want to show in the plot
     :return: perc = stored lists of the median and +/1 1sigma SFH values calculated from the gal_draws
     """
 
@@ -73,14 +74,18 @@ def stacker(gal_draws):
                 all_draws[i][note+j] += gal_draws[k][i][j]
     print(len(all_draws), len(all_draws[0]))
 
-    perc = np.zeros(shape=(len(gal_draws[0]), 3))  # len(gal_draws[0]) = 22 = len(t)
+    # perc = np.zeros(shape=(len(gal_draws[0]), 3))  # len(gal_draws[0]) = 22 = len(t)
+    perc = np.zeros(shape=(len(gal_draws[0]), 2*sigma + 1))  # len(gal_draws[0]) = 22 = len(t)
     for jj in xrange(len(gal_draws[0])):
-        perc[jj, :] = np.percentile(all_draws[jj, :], [16.0, 50.0, 84.0])  # median, +/- 34% = +/- 1sigma
+        if sigma == 1:
+            perc[jj, :] = np.percentile(all_draws[jj, :], [16.0, 50.0, 84.0])  # median, +/- 34% = +/- 1sigma
+        elif sigma == 3:
+            perc[jj, :] = np.percentile(all_draws[jj, :], [0.3, 2.4, 16.0, 50.0, 84.0, 97.6, 99.7])  # out to 3 sigma
 
     return perc  # len(perc) = 22, len(perc[0]) = 3
 
 
-def plot_sfhs(percs, t, lw=1, spec=True):
+def plot_sfhs(percs, t, lw=1, spec=True, sigma=1):
     """
     Plots SFH stacks for two different galaxy samples side-by-side
 
@@ -88,39 +93,64 @@ def plot_sfhs(percs, t, lw=1, spec=True):
     :param t: time vector output by randraw
     :param lw: line width
     :param spec: if stacking specific SFR instead of plain SFR, spec=True
+    :param sigma: how many sigma of error we want to show in the plot
     :return: plot
     """
+    ymin, ymax = 1e-2, 1e3
+    label = r'Stacked SFH [M$_\odot$ yr$^{-1}$]'
+
     if spec:
-        ymin, ymax = 1e-12, 1e-7
-    else:
-        ymin, ymax = 1e-2, 1e3
+        ymin, ymax = 1e-11, 1e-7
+        label = r'Stacked sSFH [yr$^{-1}$]'
 
     fig = plt.figure()
     ax1 = plt.subplot(1, 2, 1)
+    ax2 = plt.subplot(1, 2, 2)  # , sharey=ax1, sharex=ax1)  # don't need to share axis if plot same region & never zoom
+
+    if sigma == 1:
+        ax1.plot(t, percs[0][:, 1], '-', color='k', lw=lw)  # median
+        ax1.fill_between(t, percs[0][:, 0], percs[0][:, 2], color='k', alpha=0.3)  # fill region between +/- 1sigma
+        ax1.plot(t, percs[0][:, 0], '-', color='k', alpha=0.3, lw=lw)  # -1sigma
+        ax1.plot(t, percs[0][:, 2], '-', color='k', alpha=0.3, lw=lw)  # +1sigma
+
+        ax2.plot(t, percs[1][:, 1], '-', color='k', lw=lw)  # median
+        ax2.fill_between(t, percs[1][:, 0], percs[1][:, 2], color='k', alpha=0.3)  # fill region between +/- 1sigma
+        ax2.plot(t, percs[1][:, 0], '-', color='k', alpha=0.3, lw=lw)  # -1sigma
+        ax2.plot(t, percs[1][:, 2], '-', color='k', alpha=0.3, lw=lw)  # +1sigma
+
+    elif sigma == 3:
+        if spec:
+            ymin, ymax = 1e-13, 1e-6
+        else:
+            ymin, ymax = 1e-3, 1e4
+        ax1.plot(t, percs[0][:, 3], '-', color='k', lw=lw)  # median
+        ax1.fill_between(t, percs[0][:, 2], percs[0][:, 4], color='k', alpha=0.3)  # fill region between +/- 1sigma
+        ax1.fill_between(t, percs[0][:, 1], percs[0][:, 5], color='k', alpha=0.3)  # fill region between +/- 2sigma
+        ax1.fill_between(t, percs[0][:, 0], percs[0][:, 6], color='k', alpha=0.3)  # fill region between +/- 3sigma
+        ax1.plot(t, percs[0][:, 0], '-', color='k', alpha=0.3, lw=lw)  # -3sigma
+        ax1.plot(t, percs[0][:, 6], '-', color='k', alpha=0.3, lw=lw)  # +3sigma
+
+        ax2.plot(t, percs[1][:, 3], '-', color='k', lw=lw)  # median
+        ax2.fill_between(t, percs[1][:, 2], percs[1][:, 4], color='k', alpha=0.3)  # fill region between +/- 1sigma
+        ax2.fill_between(t, percs[1][:, 1], percs[1][:, 5], color='k', alpha=0.3)  # fill region between +/- 2sigma
+        ax2.fill_between(t, percs[1][:, 0], percs[1][:, 6], color='k', alpha=0.3)  # fill region between +/- 3sigma
+        ax2.plot(t, percs[1][:, 0], '-', color='k', alpha=0.3, lw=lw)  # -3sigma
+        ax2.plot(t, percs[1][:, 6], '-', color='k', alpha=0.3, lw=lw)  # +3sigma
+
     ax1.set_yscale("log")
     ax1.set_xscale("log")
     ax1.set_ylim(ymin, ymax)
     ax1.set_xlim(10**-2, 13.6)
-    ax1.set_ylabel(r'Stacked sSFH [M$_\odot$ yr$^{-1}$]')
-    ax1.text(4, 10**-8, 'EELGs', fontsize=30)
+    ax1.set_ylabel(label)
     # ax1.text(4, 10**2.5, 'EELGs', fontsize=30)
+    ax1.text(1, 4 * 10 ** -8, 'EELGs', fontsize=30)
 
-    ax1.plot(t, percs[0][:, 1], '-', color='k', lw=lw)  # median
-    ax1.fill_between(t, percs[0][:, 0], percs[0][:, 2], color='k', alpha=0.3)  # fill region between +/- 1sigma
-    ax1.plot(t, percs[0][:, 0], '-', color='k', alpha=0.3, lw=lw)  # -1sigma
-    ax1.plot(t, percs[0][:, 2], '-', color='k', alpha=0.3, lw=lw)  # +1sigma
-
-    ax2 = plt.subplot(1, 2, 2)  # , sharey=ax1, sharex=ax1)  # don't need to share axis if plot same region & never zoom
-    ax2.plot(t, percs[1][:, 1], '-', color='k', lw=lw)  # median
-    ax2.fill_between(t, percs[1][:, 0], percs[1][:, 2], color='k', alpha=0.3)  # fill region between +/- 1sigma
-    ax2.plot(t, percs[1][:, 0], '-', color='k', alpha=0.3, lw=lw)  # -1sigma
-    ax2.plot(t, percs[1][:, 2], '-', color='k', alpha=0.3, lw=lw)  # +1sigma
     ax2.set_yscale("log")
     ax2.set_xscale("log")
     ax2.set_ylim(ymin, ymax)
-    ax2.set_xlim(10**-2, 13.6)
-    ax2.text(4, 10**-8, 'LBGs', fontsize=30)
+    ax2.set_xlim(10 ** -2, 13.6)
     # ax2.text(4, 10**2.5, 'LBGs', fontsize=30)
+    ax2.text(1, 4 * 10 ** -8, 'LBGs', fontsize=30)
 
     plt.setp(ax2.get_yticklabels(), visible=False)  # hide y-axis labels on right-hand subplot to prevent overlap
     plt.subplots_adjust(wspace=0.05)  # vertical whitespace (i.e. the width) between the two subplots
@@ -133,16 +163,16 @@ def plot_sfhs(percs, t, lw=1, spec=True):
 
 if __name__ == "__main__":
     f = ['cdfs', 'cosmos', 'uds']  # ZFOURGE fields
-    b = ['fixedmet', 'noelg', 'noelgduston', 'dust']  # param file bases
+    b = ['fixedmet', 'noelg', 'noelgduston', 'dust', 'fixedmetmask']  # param file bases
 
     # EELGs
     c1824 = ['1824', f[1], b[0]]
     c12105 = ['12105', f[1], b[0]]
     c16067 = ['16067', f[1], b[0]]
     c17423 = ['17423', f[1], b[0]]
-    f8941 = ['8941', f[0], b[0]]
+    f8941 = ['8941', f[0], b[4]]
     u5206 = ['5206', f[2], b[0]]
-    eelgs = [c1824, c12105, c16067, c17423, u5206]
+    eelgs = [c1824, c12105, c16067, c17423, f8941, u5206]
 
     # LBGs
     # c5029 = ['5029', f[1], b[2]]  # z~2.14, emission comment --> not typical necessarily
@@ -151,7 +181,9 @@ if __name__ == "__main__":
     f6900 = ['6900', f[0], b[0]]
     f15921 = ['15921', f[0], b[3]]
     f29430 = ['29430', f[0], b[0]]
-    lbgs = [c5843, f6900, f15921, f29430]
+    f12614 = ['12614', f[0], b[1]]
+    c4942 = ['4942', f[1], b[1]]
+    lbgs = [c4942, c5843, f6900, f12614, f15921, f29430]
 
     # QUIESCENTS
     c13110 = ['13110', f[1], b[1]]
@@ -159,28 +191,29 @@ if __name__ == "__main__":
     quis = [c13110, f20752]
 
     # START STACKING
-    t = []
+    t1 = []
     draws = []
     for glxy in eelgs:
         file = glxy[0] + '_' + glxy[1] + '_' + glxy[2] + '_extra_out.pkl'
         temp = randraw(file)
         draws.append(temp[0])
-        t.append(temp[1])
+        t1.append(temp[1])
 
-    perc1 = stacker(draws)
+    sig = 3  # what sigma error to show on plot
+    perc1 = stacker(draws, sigma=sig)
 
     draws2 = []
-    t2 = []
+    # t2 = []
     for glxy in lbgs:
         file = glxy[0] + '_' + glxy[1] + '_' + glxy[2] + '_extra_out.pkl'
         temp = randraw(file)
         draws2.append(temp[0])
-        t2.append(temp[1])
+        # t2.append(temp[1])
 
-    perc2 = stacker(draws2)
+    perc2 = stacker(draws2, sigma=sig)
 
     smooth_percs = [smooth(perc1), smooth(perc2)]
-    plot_sfhs(smooth_percs, t[0])
+    plot_sfhs(smooth_percs, t1[0], sigma=sig)
 
 '''
 # run from command line in snow environment using:
