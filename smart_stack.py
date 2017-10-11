ff2fa65ddd5e51732dfcb95fb23753eab271a3ee
jhcohn <joncohn@tamu.edu>
2017-10-11 16:34:23 -0500

    updates

--- smart_stack.py
+++ smart_stack.py
@@ -140,49 +140,6 @@ def stacker2(gal_draws, times, sigma=1, spec=True, lw=1):
     ax1.set_xscale("log")
     plt.show()
 
-    '''
-    ymin, ymax = 1e-2, 1e3
-    label = r'Stacked SFH [M$_\odot$ yr$^{-1}$]'
-
-    if spec:
-        ymin, ymax = 1e-11, 1e-7
-        label = r'Stacked sSFH [yr$^{-1}$]'
-
-    fig = plt.figure()
-    ax1 = plt.subplot(1, 2, 1)
-    ax2 = plt.subplot(1, 2, 2)  # , sharey=ax1, sharex=ax1)  # don't need to share axis if plot same region & never zoom
-
-    for i in range(len(perc)):
-        ax1.plot(times[i], perc[i][:, 1], '-', color='k', lw=lw)  # median
-        ax1.fill_between(times[i], perc[i][:, 0], perc[i][:, 2], color='k', alpha=0.3)  # fill region between +/- 1sigma
-        ax1.plot(times[i], perc[i][:, 0], '-', color='k', alpha=0.3, lw=lw)  # -1sigma
-        ax1.plot(times[i], perc[i][:, 2], '-', color='k', alpha=0.3, lw=lw)  # +1sigma
-
-    #ax1.set_yscale("log")
-    #ax1.set_xscale("log")
-    ax1.set_ylim(ymin, ymax)
-    ax1.set_xlim(10**-2, 2.5)  # (0, 2.5)  # (10**-2, 13.6)
-    ax1.set_ylabel(label)
-    # ax1.text(4, 10**2.5, 'EELGs', fontsize=30)
-    ax1.text(1, 4*10**-8, 'EELGs', fontsize=30)
-
-
-    ax2.set_yscale("log")
-    ax2.set_xscale("log")
-    ax2.set_ylim(ymin, ymax)
-    ax2.set_xlim(10**-2, 2.5)  # (0, 2.5)  # (10**-2, 13.6)
-    # ax2.text(4, 10**2.5, 'LBGs', fontsize=30)
-    ax2.text(1, 4*10**-8, 'LBGs', fontsize=30)
-
-    plt.setp(ax2.get_yticklabels(), visible=False)  # hide y-axis labels on right-hand subplot to prevent overlap
-    plt.subplots_adjust(wspace=0.05)  # vertical whitespace (i.e. the width) between the two subplots
-
-    plt.rc('xtick', labelsize=20)
-    plt.rc('ytick', labelsize=20)
-    plt.rcParams.update({'font.size': 22})
-    fig.text(0.5, 0.04, 'Lookback time [Gyr]', ha='center')
-    plt.show()
-    '''
     return perc
 
 
@@ -218,7 +175,7 @@ def stacker(gal_draws, sigma=1):
     return perc
 
 
-def plot_sfhs(percs, t, lw=1, spec=True, sigma=1):
+def plot_sfhs(percs, t, lw=1, spec=True, sigma=1, save=False, title=None):
     """
     Plots SFH stacks for two different galaxy samples side-by-side
 
@@ -291,16 +248,20 @@ def plot_sfhs(percs, t, lw=1, spec=True, sigma=1):
     plt.rc('ytick', labelsize=20)
     plt.rcParams.update({'font.size': 22})
     fig.text(0.5, 0.04, 'Lookback time [Gyr]', ha='center')
-    plt.show()
+    plt.tight_layout()
+    if save:
+        plt.savefig(title + '.png', bbox_inches='tight')
+    else:
+        plt.show()
 
 
 if __name__ == "__main__":
 
-    boot = True
+    boot = False
     others = False
     if others:
-        base = ['otherbins', 'nother']  # use for otherbins
-        folders = ['opkls/', 'nopkls/']
+        base = ['thirty', 'nth']  # base = ['otherbins', 'nother']  # use for otherbins
+        folders = ['etpkls/', 'ntpkls/']  # ['opkls/', 'nopkls/']
     else:
         base = ['fixedmet', 'noelg']  # use for fixedmet
         folders = ['pkls/', 'nmpkls/']
@@ -333,95 +294,156 @@ if __name__ == "__main__":
     lbg_list.close()
 
     if boot:
-        eelgs = bootstrap(np.asarray(eelgs))
-        lbgs = bootstrap(np.asarray(lbgs))
+        cut = 'thirty_full_max'  # 'thirty_30'
+        rangey = 30
+        newfile = open('booties/bootlists-' + str(cut) + '.txt', 'w+')
+        for num in range(rangey):
+            print('loop:', num)
+            eelgs = bootstrap(np.asarray(eelgs))
+            lbgs = bootstrap(np.asarray(lbgs))
+            eelgs = eelgs[:]  # cut
+            lbgs = lbgs[:]  # cut
+            newfile.write(str(num) + '\n \n [ ')
+            for i in range(len(eelgs)):
+                newfile.write(str(eelgs[i]) + ' ')
+            newfile.write('] \n \n')
+            newfile.write('[ ')
+            for i in range(len(lbgs)):
+                newfile.write(str(lbgs[i]) + ' ')
+            newfile.write('] \n \n')
 
-        eelgs = eelgs[:3]
-        lbgs = lbgs[:3]
+            t1 = []
+            draws = []
+            boots = []
+            nummy = 0
+            c = 0
+            for glxy in eelgs:
+                c += 1
+                file = pkls + glxy + '_extra_out.pkl'
+                if os.path.exists(file):
+                    nummy += 1
+                    temp = randraw(file)  # temp[0] lists the num=1000 random posterior samples; temp[1] = time vector
+                    draws.append(temp[0])
+                    t1.append(temp[1])
+                else:
+                    print(file)
 
-    '''
-    f = ['cdfs', 'cosmos', 'uds']  # ZFOURGE fields
-    b = ['fixedmet', 'noelg', 'noelgduston', 'dust', 'fixedmetmask', 'nother', 'otherbins']  # param file bases
+            # stacker2(draws, t1)
+            sig = 1  # what sigma error to show on plot
+            perc1 = stacker(draws, sigma=sig)
 
-    # QUIESCENTS
-    c13110 = ['13110', f[1], b[1]]
-    f20752 = ['20752', f[0], b[1]]
-    quis = [c13110, f20752]
-    '''
+            draws2 = []
+            numl = 0
+            cl = 0
+            # t2 = []
+            for glxy in lbgs:
+                cl += 1
+                file = l_pkls + glxy + '_extra_out.pkl'
+                if os.path.exists(file):
+                    numl += 1
+                    temp = randraw(file)
+                    draws2.append(temp[0])
+                else:
+                    print(file)
 
-    # START STACKING
-    t1 = []
-    draws = []
-    boots = []
-    nummy = 0
-    c = 0
-    for glxy in eelgs:
-        c += 1
-        # file = glxy[0] + '_' + glxy[1] + '_' + glxy[2] + '_extra_out.pkl'
-        file = pkls + glxy + '_extra_out.pkl'
-        if os.path.exists(file):
-            nummy += 1
-            temp = randraw(file)  # temp[0] lists the num=1000 random posterior samples; temp[1] = time vector
-            # temp = bootdraw(file)  # temp[0] lists the num=1000 random posterior samples; temp[1] = time vector
-            draws.append(temp[0])
-            # boots.append(bootstrap(temp[0]))
-            t1.append(temp[1])
-        else:
-            print(file)
+            print(nummy, c, 'nume')
+            print(numl, cl, 'numl')
 
-    # stacker2(draws, t1)
-    sig = 1  # what sigma error to show on plot
-    perc1 = stacker(draws, sigma=sig)
+            perc2 = stacker(draws2, sigma=sig)
+            smooth_percs = [smooth(perc1), smooth(perc2)]
+            print(str(num))
+            plot_sfhs(smooth_percs, t1[0], sigma=sig, save=True, title='booties/bootstrap-' + str(cut) + '_' + str(num))
+        newfile.close()
 
-    draws2 = []
-    numl = 0
-    cl = 0
-    # t2 = []
-    for glxy in lbgs:
-        cl += 1
-        # file = glxy[0] + '_' + glxy[1] + '_' + glxy[2] + '_extra_out.pkl'
-        file = l_pkls + glxy + '_extra_out.pkl'
-
-        '''
-        # separate lbgs by mass
-        us = 0
-        obj = ''
-        for i in range(len(glxy)):
-            if glxy[i] == '_':
-                us += 1
-            elif us == 0:
-                obj += glxy[i]
+    else:
+        # START STACKING
+        t1 = []
+        draws = []
+        boots = []
+        nummy = 0
+        c = 0
+        for glxy in eelgs:
+            c += 1
+            # file = glxy[0] + '_' + glxy[1] + '_' + glxy[2] + '_extra_out.pkl'
+            file = pkls + glxy + '_extra_out.pkl'
+            if os.path.exists(file):
+                nummy += 1
+                temp = randraw(file)  # temp[0] lists the num=1000 random posterior samples; temp[1] = time vector
+                # temp = bootdraw(file)  # temp[0] lists the num=1000 random posterior samples; temp[1] = time vector
+                draws.append(temp[0])
+                # boots.append(bootstrap(temp[0]))
+                t1.append(temp[1])
             else:
-                pass
-        print(obj)
-        print(flist[obj])
-        if flist[obj] == 'cdfs':
-            fout = np.loadtxt('/home/jonathan/cdfs/cdfs.v1.6.9.awk.fout')
-        elif flist[obj] == 'cosmos':
-            fout = np.loadtxt('/home/jonathan/cosmos/cosmos.v1.3.6.awk.fout')
-        elif flist[obj] == 'uds':
-            fout = np.loadtxt('/home/jonathan/uds/uds.v1.5.8.awk.fout')
+                print(file)
 
-        if os.path.exists(file) and fout[int(obj) - 1][6] > 10.:
-        '''
-        if os.path.exists(file):
-            numl += 1
-            temp = randraw(file)
-            # temp = bootdraw(file)
-            draws2.append(temp[0])
-            # t2.append(temp[1])
-        else:
-            print(file)
+        # stacker2(draws, t1)
+        sig = 1  # what sigma error to show on plot
+        perc1 = stacker(draws, sigma=sig)
 
-    perc2 = stacker(draws2, sigma=sig)
+        draws2 = []
+        numl = 0
+        cl = 0
+        # t2 = []
+        for glxy in lbgs:
+            cl += 1
+            # file = glxy[0] + '_' + glxy[1] + '_' + glxy[2] + '_extra_out.pkl'
+            file = l_pkls + glxy + '_extra_out.pkl'
 
-    # smooth_percs = perc1, perc2
-    print(nummy, c, 'nume')
-    print(numl, cl, 'numl')
-    smooth_percs = [smooth(perc1), smooth(perc2)]
-    plot_sfhs(smooth_percs, t1[0], sigma=sig)
+            if os.path.exists(file):
+                numl += 1
+                temp = randraw(file)
+                # temp = bootdraw(file)
+                draws2.append(temp[0])
+                # t2.append(temp[1])
+            else:
+                print(file)
+
+        perc2 = stacker(draws2, sigma=sig)
+
+        # smooth_percs = perc1, perc2
+        print(nummy, c, 'nume')
+        print(numl, cl, 'numl')
+        smooth_percs = [smooth(perc1), smooth(perc2)]
+        plot_sfhs(smooth_percs, t1[0], sigma=sig)
+
+
+
 
 '''
 # run from command line in snow environment using:
 python smart_stack.py
+'''
+
+'''
+f = ['cdfs', 'cosmos', 'uds']  # ZFOURGE fields
+b = ['fixedmet', 'noelg', 'noelgduston', 'dust', 'fixedmetmask', 'nother', 'otherbins']  # param file bases
+
+# QUIESCENTS
+c13110 = ['13110', f[1], b[1]]
+f20752 = ['20752', f[0], b[1]]
+quis = [c13110, f20752]
+'''
+
+'''
+# separate lbgs by mass
+for glxy in lbgs:
+us = 0
+obj = ''
+for i in range(len(glxy)):
+    if glxy[i] == '_':
+        us += 1
+    elif us == 0:
+        obj += glxy[i]
+    else:
+        pass
+print(obj)
+print(flist[obj])
+if flist[obj] == 'cdfs':
+    fout = np.loadtxt('/home/jonathan/cdfs/cdfs.v1.6.9.awk.fout')
+elif flist[obj] == 'cosmos':
+    fout = np.loadtxt('/home/jonathan/cosmos/cosmos.v1.3.6.awk.fout')
+elif flist[obj] == 'uds':
+    fout = np.loadtxt('/home/jonathan/uds/uds.v1.5.8.awk.fout')
+
+if os.path.exists(file) and fout[int(obj) - 1][6] > 10.:
 '''
