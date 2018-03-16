17ddcc0c457032430a0bb0c72f71425f285b846d
jhcohn <joncohn@tamu.edu>
2018-03-16 16:51:45 -0500

    fixes

--- emlines.py
+++ emlines.py
@@ -74,11 +74,10 @@ def measure_emlines(smooth_spec,sps,enames=None):
 '''
 pc = 3.085677581467192e18  # in cm
 dfactor_10pc = 4 * np.pi * (10 * pc) ** 2
-to_ergs = 3631e-23
+to_ergs = 3631e-23  # maggies to Jansky --> 3631, Jasnky to erg/s/cm^2/Hz -->10**-23
 
 
-def smooth_spectrum(lam,spec,sigma,
-                    minlam=0.0,maxlam=1e50):
+def smooth_spectrum(lam, spec, sigma, minlam=0.0, maxlam=1e50):
 
     '''
     ripped from Charlie Conroy's smoothspec.f90
@@ -101,15 +100,15 @@ def smooth_spectrum(lam,spec,sigma,
             spec_out[ii] = spec[ii]
         else:
             vel = (lam[ii]/lam[integrate_lam]-1)*c_kms
-            func = 1/np.sqrt(2*np.pi)/sigma * np.exp(-vel**2/2./sigma**2)
-            dx=np.abs(np.diff(vel)) # we want absolute value
-            func = func / np.trapz(func,dx=dx)
-            spec_out[ii] = np.trapz(func*spec[integrate_lam],dx=dx)
+            func = 1/np.sqrt(2*np.pi)/sigma * np.exp(-vel**2/2./sigma**2)  # s/km
+            dx = np.abs(np.diff(vel)) # we want absolute value
+            func = func / np.trapz(func, dx=dx)  # (km/s)^-1 / integral([km/s]^-1 wrt km/s)
+            spec_out[ii] = np.trapz(func * spec[integrate_lam], dx=dx)  # integral(above * spec_flam, wrt km/s)
 
     return spec_out
 
 
-def ems(param_file, out_file, enames=None):
+def ems(param_file, out_file, objname='21442', field='cdfs', enames=None):
     res, pr, model = bread.results_from(out_file)
 
     # get lnprob, based on bread.param_evol()
@@ -134,12 +133,40 @@ def ems(param_file, out_file, enames=None):
     run_params = model_setup.get_run_params(argv=sargv, **clargs)
     sps = model_setup.load_sps(**run_params)
     print('sps')
-    spec, mags, sm = model.mean_model(res['chain'][walker, iteration, :], obs=res['obs'], sps=sps)
+    # spec, mags, sm = model.mean_model(res['chain'][walker, iteration, :], obs=res['obs'], sps=sps)  # spec [maggies/Hz]
     print('mean model')
     w = sps.wavelengths
-    spec *= dfactor_10pc / constants.L_sun.cgs.value * to_ergs
-    to_flam = 3e18 / w ** 2
-    spec_flam = spec * to_flam
+
+    ### save redshift, lumdist
+    z = model.params.get('zred', np.array(0.0))
+    lumdist = model.params.get('lumdist', np.array(0.0))
+    nebinspec = model.params.get('nebemlineinspec', True)
+    model.params['zred'] = np.array(0.0)
+    if lumdist:
+        model.params['lumdist'] = np.array(1e-5)
+    if nebinspec == False:
+        model.params['nebemlineinspec'] = True
+
+    ### if we want restframe optical photometry, generate fake obs file
+    ### else generate NO obs file (don't do extra filter convolutions if not necessary)
+    obs = {'filters': [], 'wavelength': None}
+    ### calculate SED. comes out as maggies per Hz, @ 10pc
+    spec, mags, sm = model.mean_model(res['chain'][walker, iteration, :], obs=obs, sps=sps)  # maggies/Hz at 10pc
+    w = sps.wavelengths
+
+    ### reset model
+    model.params['zred'] = z
+    if lumdist:
+        model.params['lumdist'] = lumdist
+    if nebinspec == False:
+        model.params['nebemlineinspec'] = False
+
+    spec *= dfactor_10pc / constants.L_sun.cgs.value * to_ergs  # spec * cm^2 * (s/erg) * erg/maggies = s*cm^2 / Hz
+    # note erg = [g cm^2 / s^2]
+    # according to measure_restframe_properties(), above line converts to Lsun / Hz
+    to_flam = 3e18 / w ** 2  # for f_nu in erg s^-1 Hz^-1 cm^-2: to_flam [(Ang / s^-1) * (1 / Ang^2)]
+    spec_flam = spec * to_flam  # erg cm^-2 s^-1 ang^-1
+
     smooth_spec = smooth_spectrum(w, spec_flam, 250.0, minlam=3e3, maxlam=7e3)
 
     ### load fsps emission line list
@@ -165,7 +192,6 @@ def ems(param_file, out_file, enames=None):
                           'H alpha 6563', '[NII]6585'])
     ##### measure emission line flux + EQW
     out = {}
-    # print(sps.get_nebline_luminosity)  # 1e-1 to 1e-10, mostly 1e-2 to 1e-6 (big long array)
     # print(1, sps.emline_wavelengths)  # big long array, 900 to 6*1e6
     for jj in xrange(len(lines)):
 
@@ -177,19 +203,22 @@ def ems(param_file, out_file, enames=None):
 
         ### calculate luminosity (in Lsun)
         idx = fsps_name[jj] == dat['name']
-        # print(sps.params['mass'], 'mass')  # e.g. BIG23104018432.0, 10554496000.0 (~1e10)
+        # L_so = 3.84*10**33 erg/s
         print(sps.params['mass'].sum())
-        eflux = float(sps.get_nebline_luminosity[idx]*sps.params['mass'].sum() * 3.848*10**33)  # erg/s per L_sol
-        elam = float(sps.emline_wavelengths[idx])
-        print(sps.get_nebline_luminosity[idx])  # 1e-1 to 1e-10, typically ~1e-2 to 1e-6
-        print(eflux, 'eflux')
-        print(elam, 'elam')
+        eflux = float(sps.get_nebline_luminosity[idx]*sps.params['mass'].sum())  # L_sun
+        elam = float(sps.emline_wavelengths[idx])  # Angstroms!
+        print(sps.get_nebline_luminosity[idx])
+        print(eflux, 'e luminosity')  # typically 10**40 to 10**42
+        print(elam, 'elam')  # Angstroms
 
         # simple continuum estimation
-        tidx = np.abs(sps.wavelengths-elam) < 100
-        eqw = eflux / np.median(smooth_spec[tidx])
+        tidx = np.abs(sps.wavelengths-elam) < 100  # True for sps.wavelengths within 100 Angstroms of elam wavelengths
+        eqw = eflux / np.median(smooth_spec[tidx])  # (erg/s) / (erg cm^-2 s^-1 Ang^-1) = Ang * cm**2
+
+        eflux *= 3.84*10**33  # erg/s (Lum, not flux)
 
         out[lines[jj]] = {'flux': eflux, 'eqw': eqw}
+        print(out)
 
     return out
 
@@ -210,8 +239,8 @@ if __name__ == "__main__":
         files[key] = kwargs[key]
 
     if files['outname'] == 'all':
-        data = 'lbg_ids1'  # 'lbg_ids1'  # 'Comp_10.dat'
-        fold = 'out_nvary/'  # 'out_nvary/'  # 'out_evar/'
+        data = 'Comp_10.dat'  # 'lbg_ids1'  # 'Comp_10.dat'
+        fold = 'out_efico/'  # 'out_nfico/'  # 'out_efico/'
         with open(data, 'r') as comp:
             e_objs = []
             e_fs = []
@@ -242,7 +271,7 @@ if __name__ == "__main__":
                 true_field = e_fs[i]
                 true_obj = e_objs[i]
 
-                out = ems(param_file, out_file)
+                out = ems(param_file, out_file, objname=true_obj, field=true_field)
                 print(out)
 
             fluxes = []
