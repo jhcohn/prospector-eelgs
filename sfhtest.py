2c7c13bba603a3db564d6180b397bf9466b78572
jhcohn <joncohn@tamu.edu>
2018-07-08 21:45:00 -0500

    updates!

--- sfhtest.py
+++ sfhtest.py
@@ -12,7 +12,44 @@ import os
 import get_mass_dust as gmd
 import ssfr_fig3 as sfh
 from matplotlib import rc
+from scipy import stats
 np.errstate(invalid='ignore')
+
+
+def density_estimation(m1, m2, xs=[8.5, 11.5], ys=[0., 700.]):
+    X, Y = np.mgrid[xs[0]:xs[1]:100j, ys[0]:ys[1]:100j]
+    positions = np.vstack([X.ravel(), Y.ravel()])
+    values = np.vstack([m1, m2])
+    kernel = stats.gaussian_kde(values)
+    Z = np.reshape(kernel(positions).T, X.shape)
+    return X, Y, Z
+
+
+def plotter(x, y, color, errs=True, fs_text=30, fs=20, norm=7., xs=[8.5, 11.5], ys=[0., 700.], scat=True, ax=None,
+            levels=None, alpha=0.5):
+    # font={'fontname': 'Times'},
+    if errs:
+        '''
+        errx = np.percentile(x, [16., 50., 84.])
+        erry = np.percentile(y, [16., 50., 84.])
+        plt.errorbar(x=[errx[1]], y=[erry[1]], xerr=[[errx[1] - errx[0]], [errx[2] - errx[1]]],
+                     yerr=[[erry[1] - erry[0]], [erry[2] - erry[1]]], color=color, fmt='*')
+        '''
+        X, Y, Z = density_estimation(x, y, xs=xs, ys=ys)
+        # levels = np.arange(0.5, np.amax(Z), 0.02) + 0.02
+        if levels is None:
+            levels = np.arange(np.amax(Z) / norm, np.amax(Z), np.amax(Z) / norm) + (np.amax(Z) / norm)
+        else:
+            levels = [lv * np.amax(Z) for lv in levels]
+        if color == 'b':
+            cmap = 'Blues'
+        else:
+            cmap = 'Purples'
+        if ax is not None:
+            ax.contourf(X, Y, Z, levels=levels, cmap=cmap, alpha=alpha)  # [0.1, 0.2, 0.5, 1., 25.]
+        else:
+            plt.contourf(X, Y, Z, levels=levels, cmap=cmap, alpha=alpha)  # [0.1, 0.2, 0.5, 1., 25.]
+            # plt.contour(X, Y, Z, levels=levels, cmap=cmap, lw=3)#, alpha=0.5)  # [0.1, 0.2, 0.5, 1., 25.]
 
 
 if __name__ == "__main__":
@@ -60,14 +97,16 @@ if __name__ == "__main__":
     fs = 10  # 20
     fs_ticks = 15  # 25
 
+    l1, l2, l3, l4, l5, l6 = [[], []], [[], []], [[], []], [[], []], [[], []], [[], []]
+
     # get_e = np.zeros(shape=(4, len(gals)))  # 6 rows (mass, dust, logzsol, gaslogz, ssfr1, ssfr2)
     onedraw = np.zeros(shape=(200, 6, 10**3))  # *10**3))  # [mass, dust, metal, gasmet, ssfr1, ssfr2]
     # (shape=(len(gals), 6, 10**3))
     # NOTE: INPUT SAME gasmet each time
     # sfh_bins = np.zeros(shape=(len(gals), 2, 10**4))  # 10**4 = num arg in ssfr_fig3.randraw()
-    # for i in range(100):  # for each galaxy (0 through 99)
-    for ind in range(len(allkeys)):
-        i = allkeys[ind]
+    # for ind in range(len(allkeys)):
+    for i in range(100):  # for each galaxy (0 through 99)
+        # i = allkeys[ind]
         print(i, gals[str(i)])  # i = key
         if os.path.exists(oute + gals[str(i)]):
             onedraw[i, :, :] = gmd.printer(oute + gals[str(i)], percs=False, sfhtest=True, draw1=True)
@@ -93,7 +132,7 @@ if __name__ == "__main__":
                     second.append(randomdraws[j][k])
         # WAIT!!!!!!!!! COMPARING SSFR TO INPUT MFRAC ISN'T RIGHT
         '''
-        pars = git + 'bettertest/sfhtest_' + str(i) + '_params.py'
+        pars = git + 'ctest/sfhtest_' + str(i) + '_params.py'
         with open(pars, 'r') as parfile:
             for line in parfile:
                 if line.startswith("# Codename: "):
@@ -112,6 +151,17 @@ if __name__ == "__main__":
                             insfr1 += char
                         elif spaces == 6:
                             insfr2 += char
+                elif line.startswith("# Identity: "):
+                    ispaces = 0
+                    zred = ''
+                    for char in line:
+                        if char == ' ' or char == '_':
+                            ispaces += 1
+                        elif ispaces == 4:
+                            zred += char
+        zred = float(zred)
+        print(zred, 'zred')
+
         # print(onedraw[i, 0, :])
 
         # ssfr050 = np.percentile(recent, [16., 50., 84.])
@@ -124,14 +174,36 @@ if __name__ == "__main__":
         sfr1 = np.percentile(onedraw[i, 4, :], [16., 50., 84.])
         sfr2 = np.percentile(onedraw[i, 5, :], [16., 50., 84.])
 
-        ax1.errorbar(float(inmass), mass[1], yerr=np.array([[mass[1] - mass[0], mass[2] - mass[1]]]).T, fmt='ko')
-        ax2.errorbar(float(indust), dust[1], yerr=np.array([[dust[1] - dust[0], dust[2] - dust[1]]]).T, fmt='ko')
-        ax3.errorbar(10**float(inmet), 10**met[1], yerr=np.array([[10**met[1]-10**met[0], 10**met[2]-10**met[1]]]).T,
-                     fmt='ko')
-        ax4.errorbar(float(insfr1), sfr1[1], yerr=np.array([[sfr1[1] - sfr1[0], sfr1[2] - sfr1[1]]]).T, fmt='ko')
-        ax5.errorbar(float(insfr2), sfr2[1], yerr=np.array([[sfr2[1] - sfr2[0], sfr2[2] - sfr2[1]]]).T, fmt='ko')
-        ax6.errorbar(float(insfr1) / float(insfr2), sfr1[1] / sfr2[1], fmt='ko')
+        '''
+        if 2.5 <= zred < 3.:
+            color = 'b'
+        elif 3. <= zred < 3.5:
+            color = 'purple'
+        elif 3.5 < zred < 4.:
+            color = 'r'
+        # '''
+        color = 'k'
 
+        ax1.errorbar(float(inmass), mass[1], yerr=np.array([[mass[1] - mass[0], mass[2] - mass[1]]]).T, fmt='o', color=color)
+        ax2.errorbar(float(indust), dust[1], yerr=np.array([[dust[1] - dust[0], dust[2] - dust[1]]]).T, fmt='o', color=color)
+        ax3.errorbar(10**float(inmet), 10**met[1], yerr=np.array([[10**met[1]-10**met[0], 10**met[2]-10**met[1]]]).T,
+                     fmt='o', color=color)
+        ax4.errorbar(float(insfr1), sfr1[1], yerr=np.array([[sfr1[1] - sfr1[0], sfr1[2] - sfr1[1]]]).T, fmt='o', color=color)
+        ax5.errorbar(float(insfr2), sfr2[1], yerr=np.array([[sfr2[1] - sfr2[0], sfr2[2] - sfr2[1]]]).T, fmt='o', color=color)
+        ax6.errorbar(float(insfr1) / float(insfr2), sfr1[1] / sfr2[1], fmt='o', color=color)
+        # '''
+        l1[0].append(float(inmass))
+        l1[1].append(mass)
+        l2[0].append(float(indust))
+        l2[1].append(dust)
+        l3[0].append(10**float(inmet))
+        l3[1].append(10**met)
+        l4[0].append(float(insfr1))
+        l4[1].append(sfr1)
+        l5[0].append(float(insfr2))
+        l5[1].append(sfr2)
+        l6[0].append(float(insfr1) / float(insfr2))
+        l6[1].append(sfr1 / sfr2)
         print(float(inmass), mass[1], pars)
         print(float(insfr1), sfr1[1])
         print(float(insfr2), sfr2[1])
@@ -213,15 +285,17 @@ if __name__ == "__main__":
     ax5.set_yticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])  # technically works
     ax5.set_yticklabels([r'$0.0$', r'$0.2$', r'$0.4$', r'$0.6$', r'$0.8$', r'$1.0$'], size=fs_ticks)
 
-    ax6.set_xlim(0., 10.)
-    ax6.set_ylim(0., 10.)
-    ax6.plot([0., 10.], [0., 10.], color='k', linestyle='--')
+    ax6.set_xscale("log")
+    ax6.set_yscale("log")
+    ax6.set_xlim(10**-2., 10**2.)
+    ax6.set_ylim(10**-2., 10**2.)
+    ax6.plot([10**-2., 10**2.], [10**-2., 10**2.], color='k', linestyle='--')
     ax6.set_xlabel(r'Input ratio of M$^*$ formed [$f_1 / f_2$]', fontsize=fs_text)
     ax6.set_ylabel(r'Recovered ratio of M$^*$ formed [$f_1 / f_2$]', fontsize=fs_text)
-    ax6.set_xticks([0.0, 2., 4., 6., 8., 10.0])  # technically works
-    ax6.set_xticklabels([r'$0.0$', r'$2.0$', r'$4.0$', r'$6.0$', r'$8.0$', r'$10.0$'], size=fs_ticks)
-    ax6.set_yticks([0.0, 2., 4., 6., 8., 10.0])  # technically works
-    ax6.set_yticklabels([r'$0.0$', r'$2.0$', r'$4.0$', r'$6.0$', r'$8.0$', r'$10.0$'], size=fs_ticks)
+    ax6.set_xticks([10**-2., 10**-1., 10**0., 10**1., 10**2.])  # technically works
+    ax6.set_xticklabels([r'$10^{-2}$', r'$10^{-1}$', r'$10^{0}$', r'$10^{1}$', r'$10^{2}$'], size=fs_ticks)
+    ax6.set_yticks([10**-2., 10**-1., 10**0., 10**1., 10**2.])  # technically works
+    ax6.set_yticklabels([r'$10^{-2}$', r'$10^{-1}$', r'$10^{0}$', r'$10^{1}$', r'$10^{2}$'], size=fs_ticks)
 #    ax6.set_yticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])  # technically works
 #    ax6.set_yticklabels([r'$0.0$', r'$0.2$', r'$0.4$', r'$0.6$', r'$0.8$', r'$1.0$'], size=fs_ticks)
     plt.show()
