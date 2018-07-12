33f4c73dc7bb6debf6d150fc172b7889d0ca55ea
jhcohn <joncohn@tamu.edu>
2018-07-12 09:56:28 -0500

    before I screw everything up

--- sfhtest.py
+++ sfhtest.py
@@ -16,42 +16,6 @@ from scipy import stats
 np.errstate(invalid='ignore')
 
 
-def density_estimation(m1, m2, xs=[8.5, 11.5], ys=[0., 700.]):
-    X, Y = np.mgrid[xs[0]:xs[1]:100j, ys[0]:ys[1]:100j]
-    positions = np.vstack([X.ravel(), Y.ravel()])
-    values = np.vstack([m1, m2])
-    kernel = stats.gaussian_kde(values)
-    Z = np.reshape(kernel(positions).T, X.shape)
-    return X, Y, Z
-
-
-def plotter(x, y, color, errs=True, fs_text=30, fs=20, norm=7., xs=[8.5, 11.5], ys=[0., 700.], scat=True, ax=None,
-            levels=None, alpha=0.5):
-    # font={'fontname': 'Times'},
-    if errs:
-        '''
-        errx = np.percentile(x, [16., 50., 84.])
-        erry = np.percentile(y, [16., 50., 84.])
-        plt.errorbar(x=[errx[1]], y=[erry[1]], xerr=[[errx[1] - errx[0]], [errx[2] - errx[1]]],
-                     yerr=[[erry[1] - erry[0]], [erry[2] - erry[1]]], color=color, fmt='*')
-        '''
-        X, Y, Z = density_estimation(x, y, xs=xs, ys=ys)
-        # levels = np.arange(0.5, np.amax(Z), 0.02) + 0.02
-        if levels is None:
-            levels = np.arange(np.amax(Z) / norm, np.amax(Z), np.amax(Z) / norm) + (np.amax(Z) / norm)
-        else:
-            levels = [lv * np.amax(Z) for lv in levels]
-        if color == 'b':
-            cmap = 'Blues'
-        else:
-            cmap = 'Purples'
-        if ax is not None:
-            ax.contourf(X, Y, Z, levels=levels, cmap=cmap, alpha=alpha)  # [0.1, 0.2, 0.5, 1., 25.]
-        else:
-            plt.contourf(X, Y, Z, levels=levels, cmap=cmap, alpha=alpha)  # [0.1, 0.2, 0.5, 1., 25.]
-            # plt.contour(X, Y, Z, levels=levels, cmap=cmap, lw=3)#, alpha=0.5)  # [0.1, 0.2, 0.5, 1., 25.]
-
-
 if __name__ == "__main__":
 
     folder = 'out_sfhtest/'
@@ -100,12 +64,14 @@ if __name__ == "__main__":
     l1, l2, l3, l4, l5, l6 = [[], []], [[], []], [[], []], [[], []], [[], []], [[], []]
 
     # get_e = np.zeros(shape=(4, len(gals)))  # 6 rows (mass, dust, logzsol, gaslogz, ssfr1, ssfr2)
-    onedraw = np.zeros(shape=(200, 6, 10**3))  # *10**3))  # [mass, dust, metal, gasmet, ssfr1, ssfr2]
+    onedraw = np.zeros(shape=(100, 6, 10**3))  # *10**3))  # [mass, dust, metal, gasmet, ssfr1, ssfr2]
     # (shape=(len(gals), 6, 10**3))
     # NOTE: INPUT SAME gasmet each time
     # sfh_bins = np.zeros(shape=(len(gals), 2, 10**4))  # 10**4 = num arg in ssfr_fig3.randraw()
     # for ind in range(len(allkeys)):
-    for i in range(100):  # for each galaxy (0 through 99)
+    offsets = np.zeros(shape=(100, 6))  # [mass, dust, metal, sfh1, sfh2, sfh1/sfh2]
+    meds = np.zeros(shape=(100, 6))  # [mass, dust, metal, sfh1, sfh2, sfh1/sfh2]
+    for i in range(1,100):  # for each galaxy (0 through 99)
         # i = allkeys[ind]
         print(i, gals[str(i)])  # i = key
         if os.path.exists(oute + gals[str(i)]):
@@ -173,7 +139,23 @@ if __name__ == "__main__":
         gasmet = np.percentile(onedraw[i, 3, :], [16., 50., 84.])
         sfr1 = np.percentile(onedraw[i, 4, :], [16., 50., 84.])
         sfr2 = np.percentile(onedraw[i, 5, :], [16., 50., 84.])
+        ratio = []
+        for k in range(10**3):
+            ratio.append(onedraw[i, 4, np.random.randint(0, 999)] / onedraw[i, 5, np.random.randint(0, 999)])
+        sfr_ratio = np.percentile(ratio, [16., 50., 84.])
+        offsets[i, 0] = mass[1] - float(inmass)
+        offsets[i, 1] = dust[1] - float(indust)
+        offsets[i, 2] = 10**met[1] - 10**float(inmet)
+        offsets[i, 3] = sfr1[1] - float(insfr1)
+        offsets[i, 4] = sfr2[1] - float(insfr2)
+        offsets[i, 5] = sfr1[1] / sfr2[1] - float(insfr1) / float(insfr2)
 
+        meds[i, 0] = mass[1]
+        meds[i, 1] = dust[1]
+        meds[i, 2] = 10**met[1]
+        meds[i, 3] = sfr1[1]
+        meds[i, 4] = sfr2[1]
+        meds[i, 5] = sfr1[1] / sfr2[1]
         '''
         if 2.5 <= zred < 3.:
             color = 'b'
@@ -182,15 +164,21 @@ if __name__ == "__main__":
         elif 3.5 < zred < 4.:
             color = 'r'
         # '''
-        color = 'k'
+        color = 'b'
 
-        ax1.errorbar(float(inmass), mass[1], yerr=np.array([[mass[1] - mass[0], mass[2] - mass[1]]]).T, fmt='o', color=color)
-        ax2.errorbar(float(indust), dust[1], yerr=np.array([[dust[1] - dust[0], dust[2] - dust[1]]]).T, fmt='o', color=color)
+        ax1.errorbar(float(inmass), mass[1], yerr=np.array([[mass[1] - mass[0], mass[2] - mass[1]]]).T, fmt='o',
+                     color=color)
+        ax2.errorbar(float(indust), dust[1], yerr=np.array([[dust[1] - dust[0], dust[2] - dust[1]]]).T, fmt='o',
+                     color=color)
         ax3.errorbar(10**float(inmet), 10**met[1], yerr=np.array([[10**met[1]-10**met[0], 10**met[2]-10**met[1]]]).T,
                      fmt='o', color=color)
-        ax4.errorbar(float(insfr1), sfr1[1], yerr=np.array([[sfr1[1] - sfr1[0], sfr1[2] - sfr1[1]]]).T, fmt='o', color=color)
-        ax5.errorbar(float(insfr2), sfr2[1], yerr=np.array([[sfr2[1] - sfr2[0], sfr2[2] - sfr2[1]]]).T, fmt='o', color=color)
-        ax6.errorbar(float(insfr1) / float(insfr2), sfr1[1] / sfr2[1], fmt='o', color=color)
+        ax4.errorbar(float(insfr1), sfr1[1], yerr=np.array([[sfr1[1] - sfr1[0], sfr1[2] - sfr1[1]]]).T, fmt='o',
+                     color=color)
+        ax5.errorbar(float(insfr2), sfr2[1], yerr=np.array([[sfr2[1] - sfr2[0], sfr2[2] - sfr2[1]]]).T, fmt='o',
+                     color=color)
+        ax6.errorbar(float(insfr1) / float(insfr2), sfr_ratio[1],
+                     yerr=np.array([[sfr_ratio[1] - sfr_ratio[0], sfr_ratio[2] - sfr_ratio[1]]]).T, fmt='o',
+                     color=color)
         # '''
         l1[0].append(float(inmass))
         l1[1].append(mass)
@@ -215,6 +203,10 @@ if __name__ == "__main__":
         print('met', 10**met[1], '+', 10**met[2]-10**met[1], '-', 10**met[1]-10**met[0])
         print('gasmet', 10**gasmet[1], '+', 10**gasmet[2]-10**gasmet[1], '-', 10**gasmet[1]-10**gasmet[0])
 
+    things = ['mass', 'dust', 'met', 'ssf1','ssfr2', 'ratio']
+    for j in range(len(offsets[0])):  # for all 5 recorded parameter offsets
+        print(np.percentile(offsets[:, j], [16., 50., 84]), 'percentiles offset')
+        print(np.mean(offsets[:, j]), things[j], 'mean offset')
     '''
     ax1.tick_params('x', length=3, width=1, which='both', labelsize=fs, pad=10)
     ax1.tick_params('y', length=3, width=0.5, which='both', labelsize=fs)
@@ -222,18 +214,118 @@ if __name__ == "__main__":
     ax1.set_xticks([10 ** 3, 2 * 10 ** 3, 5 * 10 ** 3, 10 ** 4, 2 * 10 ** 4])  # technically works
     ax1.set_xticklabels([r'$10^3$', r'$2\times10^3$', r'$5 \times 10^3$', r'$10^4$', r'$2\times10^4$'], size=fs_ticks)
     '''
+
+    # EELG-LIKE RECOVERY
+    efold = 'out_eetest/'
+    egals = {}
+    eoute = '/home/jonathan/.conda/envs/snowflakes/lib/python2.7/site-packages/prospector/git/out/' + efold
+    # pkls = git + pkl
+    ekeys = []
+    for file in os.listdir(eoute):
+        c = 0
+        key = ''
+        for char in file:
+            if char == '_':
+                c += 1
+            elif c == 3:
+                key += char
+        if file.endswith(".h5") and float(key) < 200:
+            egals[key] = file
+            ekeys.append(key)
+    eonedraw = np.zeros(shape=(1, 6, 10 ** 3))  # *10**3))  # [mass, dust, metal, gasmet, ssfr1, ssfr2]
+    eoffsets = np.zeros(shape=(1, 6))  # [mass, dust, metal, sfh1, sfh2, sfh1/sfh2]
+    emeds = np.zeros(shape=(1, 6))  # [mass, dust, metal, sfh1, sfh2, sfh1/sfh2]
+    for i in range(len(egals)):  # for each galaxy (0 through 99)
+        # i = allkeys[ind]
+        print(i, egals[str(i)])  # i = key
+        if os.path.exists(eoute + egals[str(i)]):
+            eonedraw[i, :, :] = gmd.printer(eoute + egals[str(i)], percs=False, sfhtest=True, draw1=True)
+        pars = git + 'eetest/sfhtest_' + str(i) + '_params.py'
+        with open(pars, 'r') as parfile:
+            for line in parfile:
+                if line.startswith("# Codename: "):
+                    spaces = 0
+                    inmass, indust, inmet, insfr1, insfr2 = '', '', '', '', ''
+                    for char in line:
+                        if char == ' ' or char == '_':
+                            spaces += 1
+                        elif spaces == 2:
+                            inmass += char
+                        elif spaces == 3:
+                            indust += char
+                        elif spaces == 4:
+                            inmet += char
+                        elif spaces == 5:
+                            insfr1 += char
+                        elif spaces == 6:
+                            insfr2 += char
+                elif line.startswith("# Identity: "):
+                    ispaces = 0
+                    zred = ''
+                    for char in line:
+                        if char == ' ' or char == '_':
+                            ispaces += 1
+                        elif ispaces == 4:
+                            zred += char
+        zred = float(zred)
+        print(zred, 'zred')
+        mass = np.percentile(eonedraw[i, 0, :], [16., 50., 84.])
+        # print(mass, 'mass')
+        dust = np.percentile(eonedraw[i, 1, :], [16., 50., 84.])
+        met = np.percentile(eonedraw[i, 2, :], [16., 50., 84.])
+        gasmet = np.percentile(eonedraw[i, 3, :], [16., 50., 84.])
+        sfr1 = np.percentile(eonedraw[i, 4, :], [16., 50., 84.])
+        sfr2 = np.percentile(eonedraw[i, 5, :], [16., 50., 84.])
+        ratio = []
+        for k in range(10**3):
+            ratio.append(eonedraw[i, 4, np.random.randint(0, 999)] / eonedraw[i, 5, np.random.randint(0, 999)])
+        sfr_ratio = np.percentile(ratio, [16., 50., 84.])
+        eoffsets[i, 0] = mass[1] - float(inmass)
+        eoffsets[i, 1] = dust[1] - float(indust)
+        eoffsets[i, 2] = 10 ** met[1] - 10 ** float(inmet)
+        eoffsets[i, 3] = sfr1[1] - float(insfr1)
+        eoffsets[i, 4] = sfr2[1] - float(insfr2)
+        eoffsets[i, 5] = sfr1[1] / sfr2[1] - float(insfr1) / float(insfr2)
+        emeds[i, 0] = mass[1]
+        emeds[i, 1] = dust[1]
+        emeds[i, 2] = 10 ** met[1]
+        emeds[i, 3] = sfr1[1]
+        emeds[i, 4] = sfr2[1]
+        emeds[i, 5] = sfr1[1] / sfr2[1]
+        color = 'purple'
+        fmt = '*'
+        sz = 20
+        ax1.errorbar(float(inmass), mass[1], yerr=np.array([[mass[1] - mass[0], mass[2] - mass[1]]]).T, fmt='*',
+                     color=color, markersize=sz)
+        ax2.errorbar(float(indust), dust[1], yerr=np.array([[dust[1] - dust[0], dust[2] - dust[1]]]).T, fmt='*',
+                     color=color, markersize=sz)
+        ax3.errorbar(10 ** float(inmet), 10 ** met[1],
+                     yerr=np.array([[10 ** met[1] - 10 ** met[0], 10 ** met[2] - 10 ** met[1]]]).T,
+                     fmt='*', color=color, markersize=sz)
+        ax4.errorbar(float(insfr1), sfr1[1], yerr=np.array([[sfr1[1] - sfr1[0], sfr1[2] - sfr1[1]]]).T, fmt='*',
+                     color=color, markersize=sz)
+        ax5.errorbar(float(insfr2), sfr2[1], yerr=np.array([[sfr2[1] - sfr2[0], sfr2[2] - sfr2[1]]]).T, fmt='*',
+                     color=color, markersize=sz)
+        ax6.errorbar(float(insfr1) / float(insfr2), sfr_ratio[1],
+                     yerr=np.array([[sfr_ratio[1] - sfr_ratio[0], sfr_ratio[2] - sfr_ratio[1]]]).T, fmt='*',
+                     color=color, markersize=sz)
+
     # SET UP AXES:
     axes = [ax1, ax2, ax3, ax4, ax5]
     for ax in axes:
         ax.tick_params('x', length=3, width=1, which='both', labelsize=fs, pad=10)
         ax.tick_params('y', length=3, width=0.5, which='both', labelsize=fs)
 
+    line1 = np.polyfit(l1[0], l1[1], deg=1)
+    print(line1, 'line1')
+    print(len(l1), len(l1[0]), len(l1[1]))
+
     # MASS AXES
     ax1.set_xlim(8., 11.)
     ax1.set_ylim(8., 11.)
     ax1.plot([8., 11.], [8., 11.], color='k', linestyle='--')
-    ax1.set_xlabel(r'Input stellar mass [$\log_{10}($M/M$_\odot$)]', fontsize=fs_text)
-    ax1.set_ylabel(r'Recovered stellar mass [$\log_{10}($M/M$_\odot$)]', fontsize=fs_text)
+    ax1.set_xlabel(r'Input $\log_{10}($M/M$_\odot$)', fontsize=fs_text)
+    ax1.set_ylabel(r'Recovered $\log_{10}($M/M$_\odot$)', fontsize=fs_text)
     ax1.set_xticks([8.0, 8.5, 9.0, 9.5, 10.0, 10.5, 11.0])  # technically works
     ax1.set_xticklabels([r'$8.0$', r'$8.5$', r'$9.0$', r'$9.5$', r'$10.0$', r'$10.5$', r'$11.0$'], size=fs_ticks)
     ax1.set_yticks([8.0, 8.5, 9.0, 9.5, 10.0, 10.5, 11.0])  # technically works
@@ -256,8 +348,8 @@ if __name__ == "__main__":
     ax3.set_xlim(10**-2., 1.)
     ax3.set_ylim(10**-2., 1.)
     ax3.plot([10**-2., 1.], [10**-2., 1.], color='k', linestyle='--')
-    ax3.set_xlabel(r'Input stellar mettalicity [$\log_{10}($Z/Z$_\odot$)]', fontsize=fs_text)
-    ax3.set_ylabel(r'Recovered stellar mettalicity [$\log_{10}($Z/Z$_\odot$)]', fontsize=fs_text)
+    ax3.set_xlabel(r'Input $\log_{10}($Z$^*/$Z$_\odot$)', fontsize=fs_text)
+    ax3.set_ylabel(r'Recovered $\log_{10}($Z$^*/$Z$_\odot$)', fontsize=fs_text)
     ax3.set_xticks([10 ** -2, 10 ** -1, 10 ** 0])  # technically works
     ax3.set_xticklabels([r'$10^{-2}$', r'$10^{-1}$', r'$10^0$'], size=fs_ticks)
     ax3.set_yticks([10 ** -2, 10 ** -1, 10 ** 0])  # technically works
@@ -267,8 +359,8 @@ if __name__ == "__main__":
     ax4.set_xlim(0., 1.)
     ax4.set_ylim(0., 1.)
     ax4.plot([0., 1.], [0., 1.], color='k', linestyle='--')
-    ax4.set_xlabel(r'Input fraction of M$^*$ formed [$0 - 50$ Myr]', fontsize=fs_text)
-    ax4.set_ylabel(r'Recovered fraction of M$^*$ formed [$0 - 50$ Myr]', fontsize=fs_text)
+    ax4.set_xlabel(r'Input SFH [$0 - 50$ Myr]', fontsize=fs_text)
+    ax4.set_ylabel(r'Recovered SFH [$0 - 50$ Myr]', fontsize=fs_text)
     ax4.set_xticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])  # technically works
     ax4.set_xticklabels([r'$0.0$', r'$0.2$', r'$0.4$', r'$0.6$', r'$0.8$', r'$1.0$'], size=fs_ticks)
     ax4.set_yticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])  # technically works
@@ -278,8 +370,8 @@ if __name__ == "__main__":
     ax5.set_xlim(0., 1.)
     ax5.set_ylim(0., 1.)
     ax5.plot([0., 1.], [0., 1.], color='k', linestyle='--')
-    ax5.set_xlabel(r'Input fraction of M$^*$ formed [$50 - 100$ Myr]', fontsize=fs_text)
-    ax5.set_ylabel(r'Recovered fraction of M$^*$ formed [$50 - 100$ Myr]', fontsize=fs_text)
+    ax5.set_xlabel(r'Input SFH [$50 - 100$ Myr]', fontsize=fs_text)
+    ax5.set_ylabel(r'Recovered SFH [$50 - 100$ Myr]', fontsize=fs_text)
     ax5.set_xticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])  # technically works
     ax5.set_xticklabels([r'$0.0$', r'$0.2$', r'$0.4$', r'$0.6$', r'$0.8$', r'$1.0$'], size=fs_ticks)
     ax5.set_yticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])  # technically works
@@ -290,8 +382,8 @@ if __name__ == "__main__":
     ax6.set_xlim(10**-2., 10**2.)
     ax6.set_ylim(10**-2., 10**2.)
     ax6.plot([10**-2., 10**2.], [10**-2., 10**2.], color='k', linestyle='--')
-    ax6.set_xlabel(r'Input ratio of M$^*$ formed [$f_1 / f_2$]', fontsize=fs_text)
-    ax6.set_ylabel(r'Recovered ratio of M$^*$ formed [$f_1 / f_2$]', fontsize=fs_text)
+    ax6.set_xlabel(r'Input SFH ratio [$f_{0-50} / f_{50-100}$]', fontsize=fs_text)
+    ax6.set_ylabel(r'Recovered SFH ratio [$f_{0-50} / f_{50-100}$]', fontsize=fs_text)
     ax6.set_xticks([10**-2., 10**-1., 10**0., 10**1., 10**2.])  # technically works
     ax6.set_xticklabels([r'$10^{-2}$', r'$10^{-1}$', r'$10^{0}$', r'$10^{1}$', r'$10^{2}$'], size=fs_ticks)
     ax6.set_yticks([10**-2., 10**-1., 10**0., 10**1., 10**2.])  # technically works
