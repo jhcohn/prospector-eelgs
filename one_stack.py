1f5b889aedc3bbd12678cddaa6d88bc1f48cf268
jhcohn <joncohn@tamu.edu>
2018-09-20 22:20:08 -0500

    updates to add modified version back to paper

--- one_stack.py
+++ one_stack.py
@@ -59,7 +59,8 @@ def draw_ssfr_from_prior(obj, fld, fig=None, axes=None, ndraw=1e4, alpha_sfh=1.0
             ssfr[i, :] = np.log10(mass[i, :] / time_per_bin[i] / 10**logmass)
         # ssfr[i, :] = np.log10(np.clip(mass[i, :].sum(axis=0) / time_per_bin[i].sum() / 10**logmass, minssfr, maxssfr))
         else:
-            ssfr[i, :] = np.log10(mass[i, :].sum(axis=0) / t / (10 ** logmass) / smass_factor)
+            # ssfr[i, :] = np.log10(mass[i, :].sum(axis=0) / t / (10 ** logmass) / smass_factor)
+            ssfr[i, :] = np.log10(mass[i, :].sum(axis=0) / t / (10 ** logmass))
 
     # print(ssfr)
     print('perc', np.percentile(ssfr[0], [16, 50, 84]))
@@ -95,7 +96,7 @@ def draw_ssfr_from_prior(obj, fld, fig=None, axes=None, ndraw=1e4, alpha_sfh=1.0
         return ssfr
 
 
-def randraw(infile, num=1000):  # num=1000
+def randraw(infile, logmass, num=1000):  # num=1000
     """
     For a given galaxy, randraw samples the posterior num times for each point in extra_output['extras']['sfh'][i]
 
@@ -106,13 +107,19 @@ def randraw(infile, num=1000):  # num=1000
     with open(infile, 'rb') as exout:
         extra_output = pickle.load(exout)
 
-    draw_from_sfh = np.zeros(shape=(len(extra_output['extras']['ssfr']), num))  # shape=(22, num)
+    # draw_from_sfh = np.zeros(shape=(len(extra_output['extras']['ssfr']), num))  # shape=(22, num)
+    draw_from_sfh = np.zeros(shape=(len(extra_output['extras']['sfh']), num))  # shape=(22, num)
     # print(len(extra_output['extras']['ssfr']), len(extra_output['extras']['ssfr'][0]))  # 22, 2000
     # print(len(draw_from_sfh), len(draw_from_sfh[0]))  # 22, num
 
+    '''  # BUCKET MASS WRONG?
     for i in range(len(extra_output['extras']['ssfr'])):  # at each of these 22 points
         for j in range(num):  # randomly draw from the ssfr posterior num times
-            draw_from_sfh[i][j] = extra_output['extras']['ssfr'][i][random.randint(0, num)]
+            draw_from_sfh[i][j] = extra_output['extras']['ssfr'][i][random.randint(0, num)] / 0.8  # BUCKET 1/0.8 here?
+    '''
+    for i in range(len(extra_output['extras']['sfh'])):  # at each of these 22 points
+        for j in range(num):  # randomly draw from the ssfr posterior num times
+            draw_from_sfh[i][j] = extra_output['extras']['sfh'][i][random.randint(0, num)] / (10**logmass) / 0.8
 
     return draw_from_sfh, extra_output['extras']['t_sfh']
 
@@ -175,8 +182,8 @@ def stacker(gal_draws, sigma=1):
     return perc
 
 
-def plot_sfhs(percs, t, lw=1, elist=None, uvj_in=False, spec=True, sigma=1, save=False, title=None,  priors=None,
-              tuniv=False, show=False):
+def plot_sfhs(percs, t, lw=1, elist=None, slist=None, spercs=None, st=None, uvj_in=False, spec=True, sigma=1,
+              save=False, title=None, priors=None, spriors=None, tuniv=False, show=False, color='purple'):
     """
     Plots SFH stacks for two different galaxy samples side-by-side
 
@@ -187,15 +194,37 @@ def plot_sfhs(percs, t, lw=1, elist=None, uvj_in=False, spec=True, sigma=1, save
     :param sigma: how many sigma of error we want to show in the plot
     :return: plot
     """
+    from matplotlib import rc
+    rc('font', **{'family': 'serif', 'serif': ['Times']})
+    rc('text', usetex=True)
+    fs = 25  # 20
+    fs_ticks = 30  # 25
+    fs_text = 35  # 30
+
     ymin, ymax = 1e-2, 1e3
     label = r'Stacked SFH [M$_\odot$ yr$^{-1}$]'
 
     if spec:
-        ymin, ymax = 3e-11, 3e-8
+        ymin, ymax = 3e-11, 2e-8
         label = r'Stacked SFR$_{bin}$ / M$_{tot}$ [yr$^{-1}$]'  # r'Stacked sSFH [yr$^{-1}$]'
 
     fig = plt.figure()
-    ax1 = plt.subplot(1, 2, 1)
+    if slist is None:
+        ax1 = plt.subplot(111)
+    else:
+        # ax1 = plt.subplot(121)
+        # ax2 = plt.subplot(122, sharey=ax1)
+        gs = plt.GridSpec(1, 2, wspace=0)
+        ax1 = plt.subplot(gs[0])
+        ax2 = plt.subplot(gs[1], sharey=ax1)
+        # ax2.set_yscale("log")
+        ax2.set_xscale("log")
+        # ax2.set_ylim(ymin, ymax)  # , ymax
+        ax2.set_xlim(10 ** -2, 2.5)  # (0, 2.5)  # (10**-2, 13.6)
+        # ax2.set_ylabel(label, fontsize=fs_text)  # 30
+        ax2.tick_params('x', length=3, width=1, which='both', pad=10, labelsize=fs_ticks)
+        # ax2.tick_params('y', length=3, width=0.5, which='both', labelsize=fs_ticks)
+        ax2.yaxis.set_visible(False)
 
     if uvj_in:  # also requires elist, llist to be not None; this insets uvj plots onto the top right of plot!
         inset_axes(ax1, width=8*0.32, height=8*0.28, loc=1)  # 20% width=3.2, height=2.8
@@ -205,23 +234,32 @@ def plot_sfhs(percs, t, lw=1, elist=None, uvj_in=False, spec=True, sigma=1, save
         # https://stackoverflow.com/questions/10824156/matplotlib-legend-location-numbers
 
     if sigma == 1:
-        ax1.plot(t, percs[:, 1], '-', color='k', lw=lw)  # median
-        ax1.fill_between(t, percs[:, 0], percs[:, 2], color='k', alpha=0.3)  # fill region between +/- 1sigma
-        ax1.plot(t, percs[:, 0], '-', color='k', alpha=0.3, lw=lw)  # -1sigma
-        ax1.plot(t, percs[:, 2], '-', color='k', alpha=0.3, lw=lw)  # +1sigma
+        print(percs[0, 0] - percs[0, 1], percs[0, 1], percs[0, 2] - percs[0, 1])
+        ax1.plot(t, percs[:, 1], '-', color=color, lw=lw)  # median
+        ax1.fill_between(t, percs[:, 0], percs[:, 2], color=color, alpha=0.3)  # fill region between +/- 1sigma
+        ax1.plot(t, percs[:, 0], '-', color=color, alpha=0.3, lw=lw)  # -1sigma
+        ax1.plot(t, percs[:, 2], '-', color=color, alpha=0.3, lw=lw)  # +1sigma
         print('yo', percs[0, 1], percs[0, 2], percs[0, 0])
+        if slist is not None:
+            print(spercs[0, 0] - spercs[0, 1], spercs[0, 1], spercs[0, 2] - spercs[0, 1])
+            ax2.plot(st, spercs[:, 1], '-', color='b', lw=lw)  # median
+            ax2.fill_between(st, spercs[:, 0], spercs[:, 2], color='b', alpha=0.3)  # fill region between +/- 1sigma
+            ax2.plot(st, spercs[:, 0], '-', color='b', alpha=0.3, lw=lw)  # -1sigma
+            ax2.plot(st, spercs[:, 2], '-', color='b', alpha=0.3, lw=lw)  # +1sigma
 
     ax1.set_yscale("log")
     ax1.set_xscale("log")
     ax1.set_ylim(ymin, ymax)  # , ymax
     ax1.set_xlim(10**-2, 2.5)  # (0, 2.5)  # (10**-2, 13.6)
-    ax1.set_ylabel(label, fontsize=20)  # 30
+    ax1.set_ylabel(label, fontsize=fs_text)  # 30
     # ax1.text(4, 10**2.5, 'EELGs', fontsize=30)
     # ax1.text(1, 4*10**-8, 'EELGs', fontsize=30)  # use if not uvj_in
 
     if priors is not None:
         if tuniv:
-            ax1.axhline(y=priors[1], color='r')  # median, +/- 1sigma for EELG prior
+            ax1.axhline(y=priors[1], color=color, linestyle='--')  # median, +/- 1sigma for EELG prior
+        if slist is not None:
+            ax2.axhline(y=spriors[1], color='b', linestyle='--')  # median, +/- 1sigma for EELG prior
         else:
             ssfr_pri = np.zeros(shape=(len(priors), 3))
             for i in range(len(priors)):  # 6
@@ -237,14 +275,16 @@ def plot_sfhs(percs, t, lw=1, elist=None, uvj_in=False, spec=True, sigma=1, save
             ax1.axhline(y=10 ** ssfr_pri[0][2], color='r')
             ax1.fill_between(fill_t, y1, y2, color='r', hatch='/', facecolor='none')  # , alpha=0.3)
 
-    plt.tight_layout()
-    plt.rc('xtick', labelsize=20)
-    plt.rc('ytick', labelsize=20)
-    plt.rcParams.update({'font.size': 20})
-    ax1.set_xlabel(r'Lookback time [Gyr]', fontsize=20)
-    ax1.tick_params('x', length=3, width=1, which='both', labelsize=20)
-    ax1.tick_params('y', length=3, width=0.5, which='both', labelsize=20)
-    # fig.text(0.5, 0.04, 'Lookback time [Gyr]', ha='center', fontsize=20)  # 30
+    # plt.tight_layout()
+    plt.rc('xtick', labelsize=fs_ticks)
+    plt.rc('ytick', labelsize=fs_ticks)
+    plt.rcParams.update({'font.size': fs_text})
+    if slist is None:
+        ax1.set_xlabel(r'Lookback time [Gyr]', fontsize=fs_text)
+    else:
+        fig.text(0.5, 0.04, 'Lookback time [Gyr]', ha='center', fontsize=fs_text)  # 30
+    ax1.tick_params('x', length=3, width=1, which='both', pad=10, labelsize=fs_ticks)
+    ax1.tick_params('y', length=3, width=0.5, which='both', labelsize=fs_ticks)
     plt.tight_layout()
     if save:
         plt.savefig(title + '.png', bbox_inches='tight')
@@ -254,16 +294,38 @@ def plot_sfhs(percs, t, lw=1, elist=None, uvj_in=False, spec=True, sigma=1, save
 
 if __name__ == "__main__":
 
-    vary = True
-    others = False
-    if vary:
+    vary = 0
+    others = 0
+    fifty = 0
+    newu = 0
+    fico = 1
+    if fico:
+        base = 'fico'
+        folders = 'pkl_efico/'
+        sfolders = 'pkl_nfico/'
+        mass = 9.4
+        smass = 10.1
+        import eelg_fifty_params as e_params
+    elif vary:
         base = 'vary'
         folders = 'pkl_evar/'
+        mass = 9.94
         import eelg_varymet_params as e_params
+    elif fifty:
+        base = 'fifty'
+        folders = 'pkl_efifty/'
+        mass = 9.84
+        import eelg_fifty_params as e_params
+    elif newu:
+        base = 'newu'
+        folders = 'pkl_enewu/'
+        mass = 9.91
+        import eelg_newu_params as e_params
     elif others:
-        base = 'thirty'
-        folders = 'etpkls/'
-        import eelg_thirty_params as e_params
+        base = 'thvary'  # thirty
+        folders = 'pkl_ethvary/'  # etpkls/
+        mass = 9.83
+        import eelg_thvary_params as e_params  # eelg_thirty_params
     else:
         base = 'fixedmet'  # use for fixedmet
         folders = 'pkls/'
@@ -272,13 +334,17 @@ if __name__ == "__main__":
     # pri = draw_ssfr_from_prior('1824', 'cosmos', ndraw=1e4, alpha_sfh=1.0, pfile=e_params, show=False)
     tun = True
     if tun:  # flat ssfr prior = 1 / t_univ, based on perc of t_univ values for each population
-        pri = [0.41772065 * 1e-9, 0.50135904 * 1e-9, 0.55399038 * 1e-9]  # USE
+        # pri = [0.41772065 * 1e-9, 0.50135904 * 1e-9, 0.55399038 * 1e-9]  # USE
+        pri = [0.41772065 * 1e-9, 0.529380625 * 1e-9, 0.55399038 * 1e-9]  # USE
+        spri = [0.40128419 * 1e-9, 0.44860297 * 1e-9, 0.56183993 * 1e-9]
         # --> t_univ prior = [1.81*1e9, 1.99*1e9, 2.39*1e9]  # (1.99 Gyr +0.4 Gyr / -0.18 Gyr)
     else:
         pri = draw_ssfr_from_prior(['1824'], ['cosmos'], ndraw=1e4, alpha_sfh=1.0, pfile=e_params, show=False,
                                    t=1.99 * 1e9)  # if short, t=1e9
-    eelg_list = open('eelg_specz_ids', 'r')
     pkls = '/home/jonathan/.conda/envs/snowflakes/lib/python2.7/site-packages/prospector/git/' + folders
+    spkls = '/home/jonathan/.conda/envs/snowflakes/lib/python2.7/site-packages/prospector/git/' + sfolders
+    '''
+    eelg_list = open('eelg_specz_ids', 'r')
     eelgs = []
     for line in eelg_list:
         if line[0] == '#':
@@ -288,6 +354,23 @@ if __name__ == "__main__":
             eelgs.append(cols[1] + '_' + cols[0] + '_' + base)  # base[0] = fixedmet (or otherbins)
     eelg_list.close()
 
+    eelg_list = open('Comp_10.dat', 'r')
+    eelgs = []
+    for line in eelg_list:
+        if line[0] == '#':
+            pass
+        else:
+            cols = line.split()
+            if int(cols[0]) - 200000 > 0:
+                eelgs.append(str(int(cols[0]) - 200000) + '_uds_' + base)  # base[1] = noelg (or nother)
+            elif int(cols[0]) - 100000 > 0:
+                eelgs.append(str(int(cols[0]) - 100000) + '_cosmos_' + base)  # base[1] = noelg (or nother)
+            else:
+                eelgs.append(str(int(cols[0])) + '_cdfs_' + base)  # base[1] = noelg (or nother)
+    eelg_list.close()
+    '''
+    import stellar_ages as sa
+    eelgs, sfgs = sa.get_gal_lists([base,base])
     # START STACKING
     t1 = []
     draws = []
@@ -295,12 +378,13 @@ if __name__ == "__main__":
     nummy = 0
     c = 0
     for glxy in eelgs:
+        print(glxy)
         c += 1
         # file = glxy[0] + '_' + glxy[1] + '_' + glxy[2] + '_extra_out.pkl'
         file = pkls + glxy + '_extra_out.pkl'
         if os.path.exists(file):
             nummy += 1
-            temp = randraw(file)  # temp[0] lists the num=1000 random posterior samples; temp[1] = time vector
+            temp = randraw(file, mass)  # temp[0] lists the num=1000 random posterior samples; temp[1] = time vector
             # temp = bootdraw(file)  # temp[0] lists the num=1000 random posterior samples; temp[1] = time vector
             draws.append(temp[0])
             # boots.append(bootstrap(temp[0]))
@@ -308,13 +392,38 @@ if __name__ == "__main__":
         else:
             print(file)
 
+    t2 = []
+    draws2 = []
+    boots2 = []
+    nummy2 = 0
+    c2 = 0
+    for glxy in sfgs:
+        print(glxy)
+        c2 += 1
+        # file = glxy[0] + '_' + glxy[1] + '_' + glxy[2] + '_extra_out.pkl'
+        file = spkls + glxy + '_extra_out.pkl'
+        if os.path.exists(file):
+            nummy2 += 1
+            temp2 = randraw(file, smass)  # temp[0] lists the num=1000 random posterior samples; temp[1] = time vector
+            # temp = bootdraw(file)  # temp[0] lists the num=1000 random posterior samples; temp[1] = time vector
+            draws2.append(temp2[0])
+            # boots.append(bootstrap(temp[0]))
+            t2.append(temp2[1])
+        else:
+            print(file)
+
+    print(len(draws), len(draws2))
     # stacker2(draws, t1)
     sig = 1  # what sigma error to show on plot
     perc1 = stacker(draws, sigma=sig)
+    perc2 = stacker(draws2, sigma=sig)
 
     print(nummy, c, 'nume')
     smooth_perc = smooth(perc1)
-    plot_sfhs(smooth_perc, t1[0], elist=eelgs, uvj_in=True, sigma=sig, priors=pri, tuniv=True)
+    smooth_perc2 = smooth(perc2)
+    # plot_sfhs(smooth_perc, t1[0], elist=eelgs, uvj_in=False, sigma=sig, priors=pri, tuniv=True)
+    plot_sfhs(smooth_perc, t1[0], elist=eelgs, slist=sfgs, spercs=smooth_perc2, st=t2[0], uvj_in=False, sigma=sig,
+              priors=pri, spriors=spri, tuniv=True)
 
 
 '''
